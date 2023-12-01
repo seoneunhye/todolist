5fc7a2f82a3e99088823604f1bd82f3d9c3a6b15
seoneunhye <ehsun@gemiso.com>
2023-12-01 17:47:25 +0900

    <chore> : vue3 연동

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+import { createApp } from "vue";
+import HelloWorld from "./components/Test.vue";
+
+const app = createApp({});
+app.component("hello-world", HelloWorld).mount("#app");
+
+require("./bootstrap");
